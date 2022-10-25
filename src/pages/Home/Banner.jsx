c01e4f96357f41a6bf52a6bfb24db30e7b75a15b
Sohag <sohaghossain7th@gmail.com>
2022-10-25 19:02:38 +0600

    Home page banner add

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-[70vh] bg-base-200" style={{ backgroundImage: `url("https://i.ibb.co/wQ7qwzw/banner-about-programming-coding-course-260nw-1887006397.webp")` ,  }}>
+            <div className="hero-content text-center">
+                <div className="max-w-lg ">
+                    <h1 className="text-5xl font-bold text-white">Welcome To Fun Code</h1>
+                     
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
